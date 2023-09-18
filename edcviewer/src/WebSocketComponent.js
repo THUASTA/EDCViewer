afa55d565f9ecf98b215b4ad2524b4825d643d24
赵若帆 <3186168293@qq.com>
2023-09-18 22:20:13 +0800

    style change

--- edcviewer/src/WebSocketComponent.js
+++ edcviewer/src/WebSocketComponent.js
@@ -7,18 +7,18 @@ const WebSocketComponent = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
 
-    const socket = new WebSocket('ws://localhost:8080'); // 替换为您的WebSocket服务器URL
+    const socket = new WebSocket('ws://localhost:8080'); // change the address to yours
 
     socket.onopen = () => {
       console.log('WebSocket connected');
     };
 
     socket.onmessage = (event) => {
-      // 接收到图像数据
-      const imageData = event.data; // 假设图像数据是二进制数据
+      
+      const imageData = event.data; 
 
-      // 解码并绘制图像到Canvas
-      const imageBlob = new Blob([imageData], { type: 'image/jpeg' }); // 根据实际数据类型进行更改
+      // transfer the data to image
+      const imageBlob = new Blob([imageData], { type: 'image/jpeg' }); 
       const imageObjectURL = URL.createObjectURL(imageBlob);
 
       const img = new Image();
@@ -27,7 +27,7 @@ const WebSocketComponent = () => {
       img.onload = () => {
         context.clearRect(0, 0, canvas.width, canvas.height);
         context.drawImage(img, 0, 0, canvas.width, canvas.height);
-        URL.revokeObjectURL(imageObjectURL); // 释放URL对象
+        URL.revokeObjectURL(imageObjectURL); 
       };
     };
 
@@ -36,14 +36,14 @@ const WebSocketComponent = () => {
     };
 
     return () => {
-      socket.close(); // 组件卸载时关闭WebSocket连接
+      socket.close(); // close the WebSocket connection when the component is unmounted
     };
   }, []);
 
   return (
     <div>
       <div>WebSocket图像</div>
-      <canvas ref={canvasRef} width={400} height={300} />
+      <canvas ref={canvasRef} width="300%" height="300%" />
     </div>
   );
 };
--- edcviewer/src/server.js
+++ edcviewer/src/server.js
@@ -5,22 +5,20 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  // 如果需要，可以在这里处理HTTP请求
+  // handle http request here
 });
 
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
-  // 当有WebSocket连接建立时
-
-  // 读取图像文件（这里假设图像文件为一个JPEG文件）
+  // read the picture (change the path to yours) 
   fs.readFile('pic.jpg', (err, data) => {
     if (err) {
       console.error(err);
       return;
     }
 
-    // 发送图像数据到前端
+    // senbd to client
     ws.send(data);
   });
 });
