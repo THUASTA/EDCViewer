8e64a487842ef30e99a58f9cc3d3fdf0c743fb2a
Nervonment <119149559+Nervonment@users.noreply.github.com>
2023-10-20 08:40:33 +0800

    move WebSocket object to App component (#6)

--- src/PlayerInfo.js
+++ src/PlayerInfo.js
@@ -2,47 +2,43 @@ import './InventoryItem'
 import './PlayerInfo.css'
 import './utils.css'
 
-import React, {useEffect} from 'react';
+import React from 'react';
 
 import InventoryItem from './InventoryItem';
 
 const PlayerInfo =
-    ({playerId, health, maxHealth, agility, strength, emerald, wool}) => {
-      return (
-          <div class = 'player-info-container'>
-          <div class = 'player-info-label'>Player{playerId}<
-              /div>
-            <div class='player-info-content-container'>
-                <div class='player-info-attr-row'>
-                    <div class='player-info-attr-container'>
-                        <div class='player-info-attr-label'>Health</div>
-          <div class = 'player-info-attr-val'> {
-            health
-          } /{maxHealth}</div >
+  ({ playerId, health, maxHealth, agility, strength, emerald, wool }) => {
+    return (
+      <div class='player-info-container'>
+        <div class='player-info-label'>Player {playerId}</div>
+        <div class='player-info-content-container'>
+          <div class='player-info-attr-row'>
+            <div class='player-info-attr-container'>
+              <div class='player-info-attr-label'>Health</div>
+              <div class='player-info-attr-val'>
+                {health}/{maxHealth}</div>
+            </div>
           </div>
-                </div><div class = 'player-info-attr-row'>
-          <div class = 'player-info-attr-container'>
-          <div class = 'player-info-attr-label'>Agility<
-              /div>
-                        <div class='player-info-attr-val'>{agility}</div>
+          <div class='player-info-attr-row'>
+            <div class='player-info-attr-container'>
+              <div class='player-info-attr-label'>Agility</div>
+              <div class='player-info-attr-val'>{agility}</div>
+            </div>
+            <div class='player-info-attr-container'>
+              <div class='player-info-attr-label'>Strength</div>
+              <div class='player-info-attr-val'>{strength}</div>
+            </div>
           </div>
-                    <div class='player-info-attr-container'>
-                        <div class='player-info-attr-label'>Strength</div>
-          <div class = 'player-info-attr-val'>{
-              strength}</div>
-                    </div>
+          <div class='player-info-inventory-container'>
+            <div class='player-info-attr-label'>Inventory</div>
+            <div class='flex'>
+              <InventoryItem item='emerald' count={emerald} />
+              <InventoryItem item='white_wool' count={wool} />
+            </div>
           </div>
-                <div class='player-info-inventory-container'>
-                    <div class='player-info-attr-label'>Inventory</div>
-          <div class = 'flex'>
-          <InventoryItem item = 'emerald' count =
-           {
-             emerald
-           } />
-                        <InventoryItem item='white_wool' count={wool} />
-          </div>
-                </div></div>
-        </div>)
-    }
+        </div>
+      </div>
+    )
+  }
 
 export default PlayerInfo;
---
+++ src/VideoStreamPlayer.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef } from 'react';
+
+const VideoStreamPlayer = ({ data, width, height }) => {
+    const canvasRef = useRef(null);
+
+    useEffect(
+        () => {
+            const canvas = canvasRef.current;
+            const context = canvas.getContext('2d');
+            // transfer the data to image
+            const imageData = data;
+            var binaryImageData = atob(imageData);
+            var blob = new Blob([new Uint8Array(binaryImageData.length).map((_, i) => binaryImageData.charCodeAt(i))], { type: 'image/jpeg' });
+            const imageBlob = new Blob([blob], { type: 'image/jpeg' });
+            const imageObjectURL = URL.createObjectURL(imageBlob);
+
+            const img = new Image();
+            img.src = imageObjectURL;
+            img.onload = () => {
+                console.log('image loaded');
+                canvas.width = width;
+                canvas.height = height;
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                context.drawImage(img, 0, 0, canvas.width, canvas.height);
+                URL.revokeObjectURL(imageObjectURL);
+            };
+
+            console.log(data);
+        }
+        ,[data, width, height]
+    );
+
+    return (
+        <div>
+            <canvas ref={canvasRef} width={'100%'} height={'100%'} />
+        </div>
+    );
+}
+
+export default VideoStreamPlayer;
