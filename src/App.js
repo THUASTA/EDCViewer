8b595626dfc28efa8282ccda13c2cdce74940541
Zhangyr2022 <zhangyr21@mails.tsinghua.edu.cn>
2023-11-22 01:04:35 +0800

    feat: add null judgement

--- src/App.js
+++ src/App.js
@@ -25,6 +25,10 @@ const App = () => {
   var calibrated1 = false;
   var calibrated2 = false;
   var message = true;
+
+  var player1Camera = 0;
+  var player2Camera = 1;
+
   function nullOrDefault(inputValue, defaultValue) {
     if (inputValue === null || inputValue === undefined || isNaN(inputValue)) {
       return defaultValue;
@@ -70,7 +74,7 @@ const App = () => {
         players: [
           {
             playerId: 0,
-            camera: {
+            camera: camera1 === "" ? undefined : {
               cameraId: camera1,
               recognition: {
                 hueCenter: hueCenter1,
@@ -83,14 +87,14 @@ const App = () => {
                 showMask: mask1
               }
             },
-            serialPort: {
-              port: port1,
-              baudrate: baudrate1
+            serialPort: port1 === "" ? undefined : {
+              portName: port1,
+              baudRate: baudrate1
             }
           },
           {
             playerId: 1,
-            camera: {
+            camera: camera2 === "" ? undefined : {
               cameraId: camera2,
               recognition: {
                 hueCenter: hueCenter2,
@@ -103,14 +107,16 @@ const App = () => {
                 showMask: mask2
               }
             },
-            serialPort: {
-              port: port2,
-              baudrate: baudrate2
+            serialPort: port2 === "" ? undefined : {
+              portName: port2,
+              baudRate: baudrate2
             }
 
           }
         ]
       }
+      if (camera1 !== "") player1Camera = camera1;
+      if (camera2 !== "") player2Camera = camera2;
       return data1;
     }
     catch {
@@ -155,7 +161,7 @@ const App = () => {
         players: [
           {
             playerId: 0,
-            camera: {
+            camera: camera1 === "" ? undefined : {
               cameraId: camera1,
               calibration: {
                 topLeft: {
@@ -186,14 +192,14 @@ const App = () => {
                 showMask: mask1
               }
             },
-            serialPort: {
-              port: port1,
-              baudrate: baudrate1
+            serialPort: port1 === "" ? undefined : {
+              portName: port1,
+              baudRate: baudrate1
             }
           },
           {
             playerId: 1,
-            camera: {
+            camera: camera2 === "" ? undefined : {
               cameraId: camera2,
               calibration: {
                 topLeft: {
@@ -224,13 +230,16 @@ const App = () => {
                 showMask: mask2
               }
             },
-            serialPort: {
-              port: port2,
-              baudrate: baudrate2
+            serialPort: port2 === "" ? undefined : {
+              portName: port2,
+              baudRate: baudrate2
             }
           }
         ]
       }
+      if (camera1 !== "") player1Camera = camera1;
+      if (camera2 !== "") player2Camera = camera2;
+
       return data2;
     }
     catch {
@@ -250,11 +259,11 @@ const App = () => {
 
       ws.onmessage = (event) => {
         const data = JSON.parse(event.data);
-        
+
         // about the data format, see https://thuasta.github.io/EDCHost/api/viewer/
         if (data.messageType === 'COMPETITION_UPDATE') {
-          setCamera1(data.cameras.find((value) => value.cameraId === 0));
-          setCamera2(data.cameras.find((value) => value.cameraId === 1));
+          setCamera1(data.cameras.find((value) => value.cameraId === player1Camera));
+          setCamera2(data.cameras.find((value) => value.cameraId === player2Camera));
           setPlayer1(data.players.find((value) => value.playerId === 0));
           setPlayer2(data.players.find((value) => value.playerId === 1));
           setMines(data.mines);
@@ -262,10 +271,6 @@ const App = () => {
         }
 
         if (data.messageType === 'HOST_CONFIGURATION_FROM_SERVER') {
-          // setCamera1(data.cameras.find((value) => value.cameraId === 1));
-          // setCamera2(data.cameras.find((value) => value.cameraId === 2));
-          // setPlayer1(data.players.find((value) => value.playerId === 1));
-          // setPlayer2(data.players.find((value) => value.playerId === 2));
           console.log(data);
           const ports = document.getElementsByClassName("port-select");
           const cameras = document.getElementsByClassName("camera-select");
@@ -276,7 +281,11 @@ const App = () => {
           Array.from(ports).forEach((portElement, index) => {
             // 清空当前选择框的选项
             portElement.innerHTML = "";
-
+            // 创建port
+            const option = document.createElement("option");
+            option.value = ""; // 假设摄像头对象有一个id属性
+            option.text = ""; // 假设摄像头对象有一个name属性
+            portElement.add(option);
             // 为选择框添加新的选项
             newPorts.forEach((port) => {
               const option = document.createElement("option");
@@ -293,7 +302,11 @@ const App = () => {
           Array.from(cameras).forEach((cameraElement, index) => {
             // 清空当前选择框的选项
             cameraElement.innerHTML = "";
-
+            // 创建空相机
+            const option = document.createElement("option");
+            option.value = ""; // 假设摄像头对象有一个id属性
+            option.text = ""; // 假设摄像头对象有一个name属性
+            cameraElement.add(option);
             // 为选择框添加新的选项
             newCameras.forEach((camera) => {
               const option = document.createElement("option");
@@ -341,6 +354,10 @@ const App = () => {
           masks[1].checked = showMask2;
           // 如果需要，你还可以设置默认值
           // port.value = newPorts[0].id; // 假设选择第一个摄像头作为默认值
+
+          // 更新player1Camera和player2Camera
+          player1Camera = data.players[0].camera.cameraId;
+          player2Camera = data.players[1].camera.cameraId;
         };
       };
 
